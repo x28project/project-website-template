5650866ebd69641ad0671d1e3aad872d2bf68e9f
X28 Project <x28project@gmail.com>
2021-06-20 03:15:05 -0400

    Initial commit

---
+++ webpack.config.html.js
@@ -0,0 +1,90 @@
+/**
+ * An object representing website data
+ * @type {{
+ *   title: string;
+ *   meta: {
+ *     author: string;
+ *     description: string;
+ *     keywords: string;
+ *   };
+ *   logo: {
+ *     alt: string;
+ *     src: string;
+ *   };
+ *   tagline: string;
+ *   projects: global.projects;
+ *   links: {
+ *     archive?: {
+ *       href: string;
+ *       title: string;
+ *     };
+ *     facebook?: {
+ *       href: string;
+ *       title: string;
+ *     };
+ *     linkedin?: {
+ *       href: string;
+ *       title: string;
+ *     };
+ *     soundcloud?: {
+ *       href: string;
+ *       title: string;
+ *     };
+ *     tumblr?: {
+ *       href: string;
+ *       title: string;
+ *     };
+ *     twitter?: {
+ *       href: string;
+ *       title: string;
+ *     };
+ *     youtube?: {
+ *       href: string;
+ *       title: string;
+ *     };
+ *   };
+ *   copyright: string;
+ * }[]}
+ */
+module.exports = {
+  title: 'Project Website Template',
+  meta: {
+    author: 'Project Website Template',
+    description: 'This is the Project Website Template',
+    keywords: 'Project Website Template',
+  },
+  logo: { alt: 'Project Website Template logo', src: '/images/logo.png' },
+  tagline: 'This is the text under the Project Website Template logo',
+  projects: global.projects,
+  links: {
+    archive: {
+      href: 'https://archive.org/#search.php?query=%22Project+Website+Template22&sort=-publicdate',
+      title: 'Internet Archive',
+    },
+    facebook: {
+      href: 'https://www.facebook.com/#projectwebsitetemplate',
+      title: 'Facebook',
+    },
+    linkedin: {
+      href: 'https://www.linkedin.com/in/#projectwebsitetemplate',
+      title: 'LinkedIn',
+    },
+    soundcloud: {
+      href: 'https://soundcloud.com/#projectwebsitetemplate',
+      title: 'SoundCloud',
+    },
+    tumblr: {
+      href: 'https://tumbler.com/#projectwebsitetemplate-blog.tumblr.com',
+      title: 'Tumblr',
+    },
+    twitter: {
+      href: 'https://twitter.com/#projectwebsitetemplate',
+      title: 'Twitter',
+    },
+    youtube: {
+      href: 'https://www.youtube.com/#projectwebsitetemplate',
+      title: 'YouTube',
+    },
+  },
+  copyright: 'Project Website Template',
+};
---
+++ webpack.config.projects.js
@@ -0,0 +1,97 @@
+/**
+ * An array of objects representing project data
+ * @type {{
+ *   id: string;
+ *   abbr: string;
+ *   cover?: {
+ *     alt: string;
+ *     src: string;
+ *     webp: string;
+ *   };
+ *   game?: {
+ *     href: string;
+ *   };
+ *   links?: [
+ *     {
+ *       href: string;
+ *       text: string;
+ *       type: string;
+ *     }
+ *   ];
+ *   iframe?: {
+ *     src: string;
+ *   };
+ *   title: string;
+ *   tracks?: [
+ *     {
+ *       href: string;
+ *       title: string;
+ *     }
+ *   ];
+ *   video?: boolean;
+ *   year: number;
+ *   zip?: string;
+ * }[]}
+ */
+module.exports = [
+  {
+    id: 'ProjectWebsiteTemplate-Album',
+    abbr: 'A',
+    cover: {
+      alt: 'Project Website Template - Album - Cover',
+      src: '/images/Project Website Template - Album - Cover.png',
+      webp: '/images/Project Website Template - Album - Cover.webp',
+    },
+    // iframe: {
+    //   src: '/#https://bandcamp.com/#EmbeddedPlayer/album=ALBUM_ID/size=large/bgcol=333333/linkcol=0f91ff/minimal=true/transparent=true/',
+    // },
+    title: 'Album',
+    tracks: [
+      {
+        href: '/#songs/Album/Project Website Template - Album - 01 First Track.mp3',
+        title: 'First Track',
+      },
+      {
+        href: '/#songs/Album/Project Website Template - Album - 02 Second Track.mp3',
+        title: 'Second Track',
+      },
+      {
+        href: '/#songs/Album/Project Website Template - Album - 03 Third Track.mp3',
+        title: 'Third Track',
+      },
+    ],
+    year: 2019,
+    zip: '/#songs/Album/Project Website Template - Album.zip',
+  },
+  {
+    id: 'VideoGame',
+    abbr: 'VG',
+    cover: {
+      alt: 'Video Game - Cover',
+      src: '/images/Video Game - Cover.png',
+      webp: '/images/Video Game - Cover.webp',
+    },
+    game: { href: '/#VideoGame' },
+    links: [
+      { href: '/#VideoGame', text: 'Play online', type: 'play' },
+      {
+        href: '/#games/Video Game 1.0 (Windows and Mac).zip',
+        text: 'Download (Mac/Windows)',
+        type: 'download',
+      },
+    ],
+    title: 'Video Game',
+    year: 2015,
+    zip: '/#games/Video Game 1.0 (Windows and Mac).zip',
+  },
+  {
+    id: 'ProjectWebsiteTemplate-EmbeddedVideo',
+    abbr: 'EV',
+    // iframe: {
+    //   src: 'https://www.youtube-nocookie.com/embed/VIDEO_ID?rel=0&amp;showinfo=0',
+    // },
+    title: 'Embedded Video',
+    video: true,
+    year: 2010,
+  },
+];
